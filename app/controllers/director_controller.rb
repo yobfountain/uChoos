a7c124bed97b96a6bf8a86d6d090d0f737c5f2f5
Brian Fountain <brian@brianfountain.com>
2011-06-25 15:51:14 -0400

    fixed render > render_to

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -10,17 +10,17 @@ class DirectorController < ApplicationController
     
     case origin
     when "001" && destination == "1"
-      redirect "/static/demo/rooms/002.xml"
+      redirect_to "/static/demo/rooms/002.xml"
     when "001" && destination == "2"
-      redirect "/static/demo/rooms/003.xml"
+      redirect_to "/static/demo/rooms/003.xml"
     when "002" && destination == "1"
-      redirect "/static/demo/rooms/001.xml"
+      redirect_to "/static/demo/rooms/001.xml"
     when "002" && destination == "2"
-      redirect "/static/demo/rooms/003.xml"
+      redirect_to "/static/demo/rooms/003.xml"
     when "003" && destination == "1"
-      redirect "/static/demo/rooms/001.xml"
+      redirect_to "/static/demo/rooms/001.xml"
     when "003" && destination == "2"
-      redirect "/static/demo/rooms/002.xml"
+      redirect_to "/static/demo/rooms/002.xml"
     end
     
   end
