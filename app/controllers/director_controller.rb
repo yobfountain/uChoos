b772fec16caf158c088e9dee18ad64d234e0ee8a
Brian Fountain <brian@brianfountain.com>
2011-06-25 15:21:03 -0400

    Tweaked index Added prototype rooms 001,002,003 Added simple router Added origin to routes

---
+++ app/controllers/director_controller.rb
@@ -0,0 +1,28 @@
+class DirectorController < ApplicationController
+  
+  skip_before_filter :verify_authenticity_token
+  
+  # DIRECTIONS = [ 001 => ['002', '003'], 002 => ['001', '003'], 003 => ['001', '002'] ]
+  
+  def route
+    origin = params[:origin]
+    destination = params[:Digits]
+    
+    case origin
+    when "001" and destination == "1"
+      redirect "/static/demo/rooms/002.xml"
+    when "001" and destination == "2"
+      redirect "/static/demo/rooms/003.xml"
+    when "002" and destination == "1"
+      redirect "/static/demo/rooms/001.xml"
+    when "002" and destination == "2"
+      redirect "/static/demo/rooms/003.xml"
+    when "003" and destination == "1"
+      redirect "/static/demo/rooms/001.xml"
+    when "003" and destination == "2"
+      redirect "/static/demo/rooms/002.xml"
+    end
+    
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Uchoos::Application.routes.draw do
+  get "director/route"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'director/route/:origin' => 'director#route'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
