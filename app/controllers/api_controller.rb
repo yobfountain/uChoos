e5ba0df94066bb94635a3c094c33451fe4b3403b
Brian Fountain <brian@brianfountain.com>
2011-06-25 22:14:24 -0400

    Changed some renders to redirect_to

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 class ApiController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+  
   def twilio_voice
     user = User.find_by_mobile_number(params[:From])
     mobile_number = params[:From]
@@ -7,12 +9,12 @@ class ApiController < ApplicationController
       user = User.new
       user.mobile_number = mobile_number
       user.save!
-      render 'static/new_user.xml'
+      redirect_to 'static/new_user.xml'
     else
       if user.last_game and user.last_scene
         render 'static/games/' + user.last_game + '/scenes/' + user.last_scene + '.xml'
       else
-        render 'static/games/1/scenes/001.xml'
+        redirect_to '/static/games/1/scenes/001.xml'
       end
     end
   end
