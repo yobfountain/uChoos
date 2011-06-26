da5ffe15ef805d3dface55df0902bc183e5a756f
Brian Fountain <brian@brianfountain.com>
2011-06-25 22:29:07 -0400

    added some more slashes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,7 +9,7 @@ class ApiController < ApplicationController
       user = User.new
       user.mobile_number = mobile_number
       user.save!
-      redirect_to 'static/new_user.xml'
+      redirect_to '/static/new_user.xml'
     else
       if user.last_game and user.last_scene
         render '/static/games/' + user.last_game + '/scenes/' + user.last_scene + '.xml'
