3eb9d2e68d2b6a400e7690c06b2f453487b9eb41
Brian Fountain <brian@brianfountain.com>
2011-07-01 01:47:10 -0400

    sanity check

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -14,23 +14,23 @@ class ApiController < ApplicationController
     elsif user.can_text.nil?
       redirect_to '/static/check_sms.xml'
     else
-      if twilio_number == STAGING_NUMBER
-        unless user.last_game or user.last_game.blank?
+      #if twilio_number == STAGING_NUMBER
+      #  unless user.last_game or user.last_game.blank?
           # TODO fix this hack once menu is set up
           redirect_to '/director/story_menu'
-          return
-        else
-          redirect_to '/director/router/' + user.last_game + '/' + user.last_scene
-          return
-        end
-      else
+      #    return
+      #  else
+      #   redirect_to '/director/router/' + user.last_game + '/' + user.last_scene
+      #    return
+      #  end
+      #else
         # TODO delete after building alternate system
-        if user.last_game and user.last_scene
-          redirect_to '/static/games/' + user.last_game + '/scenes/' + user.last_scene + '.xml'
-        else
-          redirect_to '/static/games/1/scenes/001.xml'
-        end
-      end
+       # if user.last_game and user.last_scene
+      #    redirect_to '/static/games/' + user.last_game + '/scenes/' + user.last_scene + '.xml'
+      #  else
+      #    redirect_to '/static/games/1/scenes/001.xml'
+      #  end
+      #end
     end
   end
 
