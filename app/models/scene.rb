29989b9b47f027221355d6a54fee6698b8559fb7
Brian Fountain <brian@brianfountain.com>
2011-07-02 12:03:26 -0400

    corrected choiceless? method

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -3,7 +3,7 @@ class Scene < ActiveRecord::Base
   belongs_to :story
   
   def choiceless?
-    self.option_two == nil
+    self.option_two == nil && !self.option_one.blank?
   end
   
   def final?
