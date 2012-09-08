1778c08b0b337b80155a901f9f5b2e8f00f9b681
Brian Fountain <brian@brianfountain.com>
2012-09-08 09:12:22 -0400

    removed token, added documentation

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Uchoos::Application.config.secret_token = '2c2d649a03e2be59730edb868b2b3276c138ff88470383a2d0c4972c45cebc4eb7e935790b3693879774a117c0775e8c49c65323ded88976937dfb286db691fe'
+Uchoos::Application.config.secret_token = ENV['UCHOOS_APP_SECRET_TOKEN'] || raise("Please set/generate your own secret token")
